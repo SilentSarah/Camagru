ec143086899f94639f394725ded555d39b8a264c
Sarah <hichammeftah4@gmail.com>
2025-11-25 11:29:32 +0100

    added a template config file

---
+++ src/backend/Core/Config.example.php
@@ -0,0 +1,14 @@
+<?php
+
+const DB_HOST = 'localhost';
+const DB_NAME = 'db';
+const DB_USER = 'root';
+const DB_PASS = 'root'; 
+
+const ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+];
+const ALLOWED_METHODS = [
+    'GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'
+];
+const ALLOW_CREDENTIALS = true;
