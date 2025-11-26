603f98f402b38bff730524d23bd499ff6bc8f8ed
Sarah <hichammeftah4@gmail.com>
2025-11-26 20:37:08 +0100

    Added tailwind run script for vscode task

--- run_tailwind.sh
+++ run_tailwind.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tailwindcss-linux-x64 -i ./src/frontend/index.css -o ./src/frontend/style.css --watch
+./tailwindcss-linux-x64 -i ./src/frontend/index.css -o ./src/frontend/style.css --watch
