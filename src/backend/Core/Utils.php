ed672af26ce35f9ca8bf907ab20d1ca78da25f6d
Sarah <hichammeftah4@gmail.com>
2025-12-08 15:58:14 +0100

    Added some utility functions to help with debugging and condition checking

---
+++ src/backend/Core/Utils.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ *   ██████  ▄▄▄       ██▀███   ▄▄▄       ██░ ██ 
+ * ▒██    ▒ ▒████▄    ▓██ ▒ ██▒▒████▄    ▓██░ ██▒
+ * ░ ▓██▄   ▒██  ▀█▄  ▓██ ░▄█ ▒▒██  ▀█▄  ▒██▀▀██░
+ *   ▒   ██▒░██▄▄▄▄██ ▒██▀▀█▄  ░██▄▄▄▄██ ░▓█ ░██ 
+ * ▒██████▒▒ ▓█   ▓██▒░██▓ ▒██▒ ▓█   ▓██▒░▓█▒░██▓
+ * ▒ ▒▓▒ ▒ ░ ▒▒   ▓▒█░░ ▒▓ ░▒▓░ ▒▒   ▓▒█░ ▒ ░░▒░▒
+ * ░ ░▒  ░ ░  ▒   ▒▒ ░  ░▒ ░ ▒░  ▒   ▒▒ ░ ▒ ░▒░ ░
+ *  ░  ░  ░    ░   ▒     ░░   ░   ░   ▒    ░  ░░ ░
+ * ░        ░  ░   ░           ░  ░ ░  ░  ░
+ *                                       
+ * File Created: Monday, 1st December 2025 5:57:50 pm
+ * Author: Hicham S.Meftah (hichammeftah4@gmail.com)
+ */
+
+function isSecure() {
+    return !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443;
+}
+
+function gen_server_url() {
+    return (isSecure() ? "https" : "http") . "://" . $_SERVER['SERVER_ADDR'];
+}
+
+
+// FOR DEBUGGING PURPOSES ONLY
+function log_stuff(...$stuff) {
+    file_put_contents("log.txt", implode("\n", $stuff) . "\n", FILE_APPEND);
+}
