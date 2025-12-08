8ef73a5018dde6b559beb36d1a9491b8ef19d0ed
Sarah <hichammeftah4@gmail.com>
2025-12-08 15:53:57 +0100

    Implemented init system to load defaults at router startup

--- src/backend/Core/init.php
+++ src/backend/Core/init.php
@@ -22,4 +22,6 @@ require_once __DIR__ ."/Validator.php";
 session_start();
 Database::getInstance();
 Config::setValidatorVerifyFn("email", fn($value) => filter_var($value, FILTER_VALIDATE_EMAIL));
+Config::setVerificationEmailTemplate(__DIR__ . "/../templates/EmailVerification.phtml");
+Config::setPasswordResetEmailTemplate(__DIR__ . "/../templates/PasswordRecovery.phtml");
 Validator::getInstance(Config::$VALIDATION_RULES);
