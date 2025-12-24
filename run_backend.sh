a04022e26241b0ac1a924eed38b616f5d0c47fa2
Sarah <hichammeftah4@gmail.com>
2025-12-24 14:45:08 +0100

    Implemented hotreloading in php

--- run_backend.sh
+++ run_backend.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-php -S localhost:8000 -t ./src/backend
+while true; do
+    php -S localhost:8000 -t ./src/backend &
+    PHP_PID=$!
+    inotifywait -q -r -e modify,delete,move --exclude 'static/' ./src/backend
+    kill $PHP_PID
+    wait $PHP_PID 2>/dev/null
+done
--- run_frontend.sh
+++ run_frontend.sh
@@ -1,1 +1,9 @@
-php -S localhost:3000 -t ./src/frontend
+#!/bin/bash
+while true; do
+    php -S localhost:3000 -t ./src/frontend &
+    PHP_PID=$!
+    inotifywait -q -r -e modify,create,delete,move ./src/frontend
+    kill $PHP_PID
+    wait $PHP_PID 2>/dev/null
+done
+
